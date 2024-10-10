8be0a21c92eada7e55716d1d29a0760449346644
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2024-10-09 21:16:38 -0400

    DB + extras

---
+++ Backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def index() -> dict[str, str]:
+    return {'hello': 'world'}
+
+@app.get('/about')
+async def about() -> str:
+    return 'this is an app'
+
+
+
+
+
