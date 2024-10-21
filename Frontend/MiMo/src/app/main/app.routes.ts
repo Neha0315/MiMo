72a47c80ad3e114b5c8fa9a9277019108ba39163
sydneywinner <sgw43@case.edu>
2024-10-21 13:49:28 -0400

    Updated nav bar logo

--- Frontend/MiMo/src/app/main/app.routes.ts
+++ Frontend/MiMo/src/app/main/app.routes.ts
@@ -6,7 +6,11 @@ import {ProfilePageComponent} from '../profile-page/profile-page.component';
 import {HomeComponent} from '../home/home.component';
 
 export const routes: Routes = [  
+<<<<<<< Updated upstream
     { path: '', redirectTo: '/home', pathMatch: 'full' },  // Default route to Home page
+=======
+    { path: '', redirectTo: '/home', pathMatch: 'full' },  // Default route to Posts page
+>>>>>>> Stashed changes
     { path: 'posts', component: PostsComponent },  // Route for Posts page
     { path: 'profile-page', component: ProfilePageComponent },  // Route for Profile page
     { path: 'home', component: HomeComponent },  // Route for Profile page
